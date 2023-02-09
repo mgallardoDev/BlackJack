f5f895a6ebfdd78ca84d6724c901cd14be95fb67
Marco Antonio Gallardo <magallope@gmail.com>
2023-02-10 00:19:13 +0100

    BlackJack terminado

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,63 +2,144 @@ let cards = [];
 const palos = ['C', 'D', 'H', 'S'];
 const figuras = ['Q', 'J', 'K', 'A'];
 
-const botonNuevoJuego = document.getElementById('botonNuevoJuego')
-const botonPedirCarta = document.getElementById('botonPedirCarta')
-const botonPasar = document.getElementById('botonPasar')
-const contenedorCartas = document.querySelectorAll('.jugador__cartas')
+const botonNuevoJuego = document.getElementById('botonNuevoJuego');
+const botonPedirCarta = document.getElementById('botonPedir');
+const botonPasar = document.getElementById('botonPasar');
+const contenedoresCartas = document.querySelectorAll('.jugador__cartas');
+const contenedoresPuntuacion = document.querySelectorAll('.jugador__puntos');
 
+let puntuaciones = [0, 0];
 
-
-
-let puntuacionJugador = 0,
-puntuacionOrdenador = 0;
+const inicializarJuego = () => {
+     cards = [];
+     botonPedirCarta.removeAttribute('disabled');
+     botonPasar.removeAttribute('disabled');
+     contenedoresCartas.forEach((contenedor) => (contenedor.innerHTML = ''));
+     contenedoresPuntuacion.forEach((contenedor) => (contenedor.innerText = 0));
+     puntuaciones = [0, 0];
+};
 
 const generateCards = function () {
-    for (i = 2; i <= 10; i++) {
-        for (letter of palos) {
-            cards.push(i + letter);
-        }
+     inicializarJuego();
+
+     for (i = 2; i <= 10; i++) {
+          for (letter of palos) {
+               cards.push(i + letter);
+          }
      }
 
      for (figura of figuras) {
           for (letter of palos) {
-              cards.push(figura + letter);
-            }
-        }
-        
-        cards = _.shuffle(cards);
-        
-        console.log(cards);
+               cards.push(figura + letter);
+          }
+     }
+
+     cards = _.shuffle(cards);
+
+     console.log(cards);
 };
 
-const pedir = (jugador1ordenador2) => {
-    let card = cards.pop();
-    mostrarCarta(card, jugador1ordenador2)
-    return card;
+const pedir = (jugador0ordenador1) => {
+     let card = cards.pop();
+     mostrarCarta(card, jugador0ordenador1);
+     puntuar(card, jugador0ordenador1);
+     return card;
 };
 
-const mostrarCarta = (carta, jugador1ordenador2) => {
-    console.log(contenedorCartas) 
-    console.log(jugador1ordenador2) 
-    const img = document.createElement('img')
-    img.src =`assets/img/cartas/${carta}.png`
-    img.classList=["carta"]
-    contenedorCartas[jugador1ordenador2].append(img)
-}
+const mostrarCarta = (carta, jugador0ordenador1) => {
+     const img = document.createElement('img');
+     img.src = `assets/img/cartas/${carta}.png`;
+     img.classList = ['carta'];
+     contenedoresCartas[jugador0ordenador1].append(img);
+};
+
+const puntuar = function (carta, jugador0ordenador1) {
+     puntuaciones[jugador0ordenador1] += +getValor(carta);
+     contenedoresPuntuacion[jugador0ordenador1].innerText =
+          puntuaciones[jugador0ordenador1];
+     isBiggerThan21(jugador0ordenador1)
+          ? (jugador0ordenador1 = 0 ? alertLoose() : null)
+          : isBlackJack(jugador0ordenador1)
+          ? cambiarTurno()
+          : null;
+};
+
+const isBiggerThan21 = (jugador0ordenador1) => {
+     return puntuaciones[jugador0ordenador1] > 21;
+};
+
+const isBlackJack = (jugador0ordenador1) => {
+     return puntuaciones[jugador0ordenador1] == 21;
+};
 
 const getValor = (carta) => {
-    let valor = carta.substring(0, carta.length - 1);
-    
-     return !isNaN(valor) ? valor : valor === 'A' ? 10 : 11;
-    };
-    
-    
+     let valor = carta.substring(0, carta.length - 1);
+
+     return !isNaN(valor) ? valor : valor === 'A' ? 11 : 10;
+};
+
+const cambiarTurno = () => {
+     bloquearBotones();
+     turnoOrdenador();
+};
+
+const turnoOrdenador = () => {
+     do {
+          setTimeout(pedir(1), 10);
+     } while (seguirJugando());
+     showResult();
+};
+
+const showResult = () => {
+     if (isBiggerThan21(1) || puntuaciones[1] < puntuaciones[0]) {
+          alertWin();
+          return;
+     }
+     if (puntuaciones[1] > puntuaciones[0]) {
+          alertLoose();
+          return;
+     }
+     if ((puntuaciones[1] = puntuaciones[0])) {
+          alertEmpate();
+     }
+};
+
+const seguirJugando = () => {
+     return puntuaciones[1] < 15 || puntuaciones[1] < puntuaciones[0];
+};
+
+const alertLoose = () => {
+     bloquearBotones();
+     alert('You loose');
+};
+const alertWin = () => {
+     bloquearBotones();
+     alert('You win');
+};
+const alertEmpate = () => {
+     bloquearBotones();
+     alert('Empatados!');
+};
+
+const bloquearBotones = () => {
+     botonPedirCarta.setAttribute('disabled', 'true');
+     botonPasar.setAttribute('disabled', 'true');
+};
+
 // EVENTS
 
+botonPedirCarta.addEventListener('click', () => {
+     pedir(0);
+});
+
+botonPasar.addEventListener('click', () => {
+     cambiarTurno();
+});
+
 botonNuevoJuego.addEventListener('click', () => {
-    pedir(0)
-})
-    
-    generateCards();
- 
-    
+     generateCards();
+});
+
+//ejecucion
+
+generateCards();
